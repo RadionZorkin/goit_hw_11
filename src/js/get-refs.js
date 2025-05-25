868ef6396a918991ed255fc20fe0abcf5296c668
Радіон <radion.zorkin@gmail.com>
2025-05-25 21:23:42 +0300

    Очистка репозиторія

--- src/js/get-refs.js
+++
@@ -1,8 +0,0 @@
-export default function getRefs() {
-  return {
-    searchForm: document.querySelector('.search-form'),
-    buttonSubmit: document.querySelector('.search-form button'),
-    buttonLoadMore: document.querySelector('.load-more'),
-    gallery: document.querySelector('.gallery'),
-  };
-}
